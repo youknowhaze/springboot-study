25cbed70ca70e30f00c9c5cd291846069cbe7a54
guofei <guofei@travelsky.com>
2020-11-11 11:09:50 +0800

    提交redis有序集合（zset）练习

--- springboot-doing/src/main/java/com/incredible/springbootdoing/redis/RedisHandleUtil.java
+++ springboot-doing/src/main/java/com/incredible/springbootdoing/redis/RedisHandleUtil.java
@@ -2,7 +2,7 @@ package com.incredible.springbootdoing.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.data.redis.connection.RedisZSetCommands;
 import org.springframework.data.redis.core.*;
 
 import java.util.*;
@@ -93,7 +93,7 @@ public class RedisHandleUtil {
     public void redisForSet(){
         // set集合不允许有重复数据，所以这里插入两个v1，只会有一个数据进入集合中
         redisTemplate.opsForSet().add("set-1","v1","v1","v2","v3","v4");
-        stringRedisTemplate.opsForSet().add("set-2","a1","a2","a3");
+        stringRedisTemplate.opsForSet().add("set-2","a1","v2","a3");
 
         //绑定set1集合
         BoundSetOperations setOperations = stringRedisTemplate.boundSetOps("set-1");
@@ -101,13 +101,78 @@ public class RedisHandleUtil {
         setOperations.remove("v1","v3"); //移除两个元素
         Long length = setOperations.size(); //得到set集合的长度
         Set set1 = setOperations.members(); //返回整个set集合
-
+        Set inter =  setOperations.intersect("set-2");  //得到两个集合的交集
+        setOperations.intersectAndStore("set-2","inter"); //球两个集合的交集并保存
+        Set diffSet = setOperations.diff("set-2");  //求两个集合的差集
+        setOperations.diffAndStore("set-2","diff");
+        Set union = setOperations.union("set-2");
+        setOperations.unionAndStore("set-2","union");
 
     }
 
 
+    /**
+     * 操作redis有序集合(Zset)
+     * Redis 有序集合和集合一样也是 string 类型元素的集合,且不允许重复的成员。
+     *
+     * 不同的是每个元素都会关联一个 double 类型的分数。redis 正是通过分数来为集合中的成员进行从小到大的排序。
+     *
+     * 有序集合的成员是唯一的,但分数(score)却可以重复。
+     */
+    public void redisForZset(){
+        Set<ZSetOperations.TypedTuple<String>> typedTupleSet = new HashSet<>();
 
+        for (int i = 0; i < 9; i++) {
+            double score = i*0.1;
+            ZSetOperations.TypedTuple<String> typedTuple = new DefaultTypedTuple<>("value"+i,score);
+            typedTupleSet.add(typedTuple);
+        }
+        //添加redis至
+        stringRedisTemplate.opsForZSet().add("zset-1",typedTupleSet);
+        // 绑定有序集合操作
+        BoundZSetOperations zSetOperations = stringRedisTemplate.boundZSetOps("zset-1");
+        // 新增有序集合元素
+        zSetOperations.add("value-demo",2.928);
+        // 整个有序集合的区间为【0，size-1】
+        Set<String> rangeSet = zSetOperations.range(1,6);
+        // 根据分数排序获取有序集合
+        Set<String> rangeCoreSet = zSetOperations.rangeByScore(0.2,0.5);
 
+        // 定义值的范围
+        RedisZSetCommands.Range range = new RedisZSetCommands.Range();
+        range.gt("value4");  //大于value4的分数
+        //range.gte("value4"); //大于等于value4的分数
+        //range.lt("value8");  //小于value8的分数
+        range.lte("value8"); //小于等于value8的分数
+        // 得到一个有序集合，这个有序集合是按照字符串排序的
+        Set<String> setLex = zSetOperations.rangeByLex(range);
+        // 移除有序集合中的元素
+        zSetOperations.remove("value-demo","value1");
+        //得到分数
+        zSetOperations.score("value8");
+        //在下标区间下，按照分数排序，同时返回value和score;
+        Set<ZSetOperations.TypedTuple<String>> set01 = zSetOperations.rangeWithScores(2,6);
+        // 在分数区间下，按分数排序，同时返回value和score
+        Set<ZSetOperations.TypedTuple<String>> set02 = zSetOperations.rangeByScoreWithScores(0.2,0.8);
+        //按下标区间进行从大到小的排序
+        Set<String> reverseSet = zSetOperations.reverseRange(2,6);
 
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
